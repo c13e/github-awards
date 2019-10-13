d251849bd74d04bb7b5fbed65533feec41864872
Christian Muhire <chris@next45.co>
2019-10-13 18:57:15 +0200

    update client to fetch users and repositories separately

--- extras/models/github_client.rb
+++ extras/models/github_client.rb
@@ -1,35 +1,54 @@
 class Models::GithubClient
   attr_accessor :max_list_size
   attr_accessor :on_found_object, :on_error, :on_too_many_requests
-  
+
   def initialize(token=nil)
     @client = Octokit::Client.new(access_token: token)
-    @max_list_size = 100
+    @max_list_size = 1000
+    @current_result_size = 0
+    @current_page = 1
   end
-  
-  def list(method, since)
+
+  def fetch_users(method, query)
     loop do
-      results = api_call(method, {since: since})
+      args = [query, { sort: 'joined', order: 'asc', per_page: 100, page: @current_page }]
+      Rails.logger.info "querying for page: #{@current_page}"
+      results = api_call(method, args)
       next if results.nil?
-      
-      Rails.logger.info "found #{results.size} objects starting at #{since}"
-      results.each do |object|
+
+      results.items.each do |object|
         on_found_object.call(object)
       end
-      since = results.last[:id]
-      break if results.size < max_list_size
+
+      @current_result_size += results.items.size
+      Rails.logger.info "Current total at #{@current_result_size} users"
+      break if @current_result_size >= @max_list_size
+      @current_page += 1
     end
   end
-  
+
+  def fetch_repositories(method)
+    User.find_each do |user|
+      results = api_call(method, [user.login])
+      next if results.nil?
+
+      Rails.logger.info "found #{results.size} objects"
+
+      results.each do |object|
+        on_found_object.call(object, user)
+      end
+    end
+  end
+
   def get(method, params)
     api_call(method, params)
   end
-  
-  private 
-  
-  def api_call(method, params)
+
+  private
+
+  def api_call(method, args)
     begin
-      @client.send(method, params)
+      @client.send(method, *args)
     rescue Octokit::TooManyRequests => e
       on_too_many_requests.call(e) if on_too_many_requests
     rescue StandardError => e
