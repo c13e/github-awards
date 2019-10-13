b8ff2d06a39ebafdc3ea7209148da601fe5c183c
Christian Muhire <chris@next45.co>
2019-10-13 18:57:45 +0200

    simplify importers

--- extras/tasks/repository_importer.rb
+++ extras/tasks/repository_importer.rb
@@ -1,19 +1,19 @@
 class Tasks::RepositoryImporter
-  def crawl_github_repos(since)
+  def crawl_github_repos
     client = Models::GithubClient.new(ENV["GITHUB_TOKEN"])
-    client.on_found_object = lambda do |repo| 
+    client.on_found_object = lambda do |repo, user|
       Repository.create(github_id: repo["id"],
-        name: repo["name"], 
-        user: User.where(login: repo["owner"]["login"]).first,
+        name: repo["name"],
+        user: user,
         forked: repo["fork"] || false)
     end
-    
+
     client.on_too_many_requests = lambda do |error|
       Rails.logger.error error
       sleep 10
       return nil
     end
-    
-    client.list(:all_repositories, since)
+
+    client.fetch_repositories(:repositories)
   end
 end
--- extras/tasks/user_importer.rb
+++ extras/tasks/user_importer.rb
@@ -1,19 +1,20 @@
 class Tasks::UserImporter
-  def crawl_github_users(since)
+  def crawl_github_users(location:)
     client = Models::GithubClient.new(ENV["GITHUB_TOKEN"])
-    client.on_found_object = lambda do |user| 
+    client.on_found_object = lambda do |user|
       User.create(github_id: user["id"],
         login: user["login"],
         gravatar_url: user["avatar_url"],
         organization: user["type"]=="Organization")
     end
-    
+
     client.on_too_many_requests = lambda do |error|
       Rails.logger.error error
       sleep 10
       return nil
     end
-    
-    client.list(:all_users, since)
+
+    query = "location:#{location}"
+    client.fetch_users(:search_users, query)
   end
 end
