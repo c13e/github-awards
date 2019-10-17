d34644202b97004965ba88e93c3c7bc2ee8da916
Christian Muhire <chris@next45.co>
2019-10-17 21:41:34 +0200

    enable runtime metrics collection

--- config/initializers/datadog.rb
+++ config/initializers/datadog.rb
@@ -1,11 +1,11 @@
 require 'datadog/statsd'
+require 'ddtrace'
 
 Datadog.configure do |c|
+  # To enable runtime metrics collection, set `true`. Defaults to `false`
+  # You can also set DD_RUNTIME_METRICS_ENABLED=true to configure this.
+  c.runtime_metrics_enabled = true
+
   c.use :rails, service_name: 'rails-app'
   c.use :sidekiq
 end
-
-$statsd = Datadog::Statsd.new('localhost', 8125)
-
-# Increment a counter.
-$statsd.increment('system.startup')
