617f70b198d1ba5c023e84db61034082c19f616f
Christian Muhire <chris@next45.co>
2019-10-17 22:54:59 +0200

    manually patch Sidekiq

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,5 +1,8 @@
+require 'ddtrace/contrib/sidekiq/server_tracer'
+
 Sidekiq.configure_server do |config|
   config.server_middleware do |chain|
+    chain.add(Sidekiq::ServerTracer)
     chain.add Sidekiq::Throttler, storage: :redis
   end
 end
